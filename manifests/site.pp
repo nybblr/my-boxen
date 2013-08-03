8166639748882aa5fc46cb8671c29a90047cb093
Jonathan Martin <me@nybblr.com>
2013-08-03 11:40:30 -0400

    Reinclude git.

--- manifests/site.pp
+++ manifests/site.pp
@@ -54,7 +54,7 @@ Homebrew::Formula <| |> -> Package <| |>
 node default {
   # core modules, needed for most things
   include gcc
-  # include git
+  include git
   include homebrew
   include xquartz
 
